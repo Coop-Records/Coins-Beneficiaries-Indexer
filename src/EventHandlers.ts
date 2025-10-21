c8bbb7c33655cc91cb10b18b531706e16e8ac6df
Sweets Sweetman <sweetmantech@gmail.com>
2025-10-21 10:30:23 -0500

    New Indexer for Beneficiaries

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -4,7 +4,10 @@
 import {
   LockableUniswapV3Initializer,
   LockableUniswapV3Initializer_Collect,
+  LockableUniswapV3Initializer_Lock,
+  BeneficiaryData,
 } from "generated";
+import { decodeAbiParameters } from "viem";
 
 LockableUniswapV3Initializer.Collect.handler(async ({ event, context }) => {
   const entity: LockableUniswapV3Initializer_Collect = {
@@ -17,4 +20,45 @@ LockableUniswapV3Initializer.Collect.handler(async ({ event, context }) => {
     blockNumber: event.block.number,
   };
   context.LockableUniswapV3Initializer_Collect.set(entity);
+});
+
+LockableUniswapV3Initializer.Lock.handler(async ({ event, context }) => {
+  console.log("Processing Lock event:", {
+    transactionHash: event.transaction.hash,
+    blockNumber: event.block.number,
+    pool: event.params.pool,
+    beneficiariesData: event.params.beneficiaries,
+  });
+
+  const lockId = `${event.chainId}_${event.block.number}_${event.logIndex}`;
+
+  // Create the Lock entity
+  const lockEntity: LockableUniswapV3Initializer_Lock = {
+    id: lockId,
+    pool: event.params.pool,
+    transactionHash: event.transaction.hash,
+    blockNumber: event.block.number,
+  };
+  context.LockableUniswapV3Initializer_Lock.set(lockEntity);
+
+  // Process the beneficiaries tuple array
+  event.params.beneficiaries.forEach((beneficiary: any, index: number) => {
+    const beneficiaryEntity: BeneficiaryData = {
+      id: `${lockId}_${index}`,
+      lockId: lockId,
+      pool: event.params.pool,
+      beneficiary: beneficiary[0], // First element is beneficiary address
+      shares: beneficiary[1], // Second element is shares
+      transactionHash: event.transaction.hash,
+      blockNumber: event.block.number,
+    };
+    context.BeneficiaryData.set(beneficiaryEntity);
+  });
+
+  console.log("Created Lock entity:", lockEntity);
+  console.log(
+    "Created",
+    event.params.beneficiaries.length,
+    "BeneficiaryData entities"
+  );
 });
